fc26099264c5948cb023054239a5f76774c95fd7
Hilton Santos <jhilton930@gmail.com>
2020-10-05 15:06:48 -0300

    Update myapp

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -30,3 +30,52 @@ class Car(models.Model):
         Manufacturer, on_delete=models.CASCADE, related_name='cars')
 
 
+class Topping(models.Model):
+    description = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.description
+
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=50)
+    topping = models.ManyToManyField(Topping)
+
+    def __str__(self):
+        return self.name
+
+
+class CPF(models.Model):
+    number = models.CharField(max_length=9)
+
+
+class personPhysical(models.Model):
+    name = models.CharField(max_length=50)
+    cpf = models.OneToOneField(
+        CPF, related_name='pessoa_fisica', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+
+class personMember(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Group(models.Model):
+    name = models.CharField(max_length=50)
+    members = models.ManyToManyField(personMember, through='Membership')
+    
+    def __str__(self):
+        return self.name
+    
+
+class Membership(models.Model):
+    person = models.ForeignKey(personMember, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined = models.DateField()
+    
+    invite_reason = models.CharField(max_length=100)
